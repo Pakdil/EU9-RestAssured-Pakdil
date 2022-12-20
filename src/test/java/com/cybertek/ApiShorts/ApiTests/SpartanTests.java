c755d18795a53f443f4798253fefcd5cd56852cb
pakdil <eaasbb@gmail.com>
2022-12-20 13:41:29 +0300

    SpartanTests status code body header

--- src/test/java/com/cybertek/ApiShorts/ApiTests/SpartanTests.java
+++ src/test/java/com/cybertek/ApiShorts/ApiTests/SpartanTests.java
@@ -1,7 +1,9 @@
 package com.cybertek.ApiShorts.ApiTests;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SpartanTests {
@@ -18,5 +20,30 @@ public class SpartanTests {
         //print body
 
         System.out.println(response.body().prettyPrint());
+    }
+
+    @Test
+    public void viewSpartanTest2() {
+        Response response = RestAssured.get(spartanBaseUrl + "/api/spartans");
+
+        // verify status code 200
+        Assert.assertEquals(response.getStatusCode(), 200);
+
+        //verify body contains Allen
+        Assert.assertTrue(response.body().asString().contains("Allen"));
+
+    }
+
+    @Test
+    public void viewSpartanTest3() {
+      Response response =  RestAssured.given().accept(ContentType.XML)
+                .when().get(spartanBaseUrl + "/api/spartans");
+
+        Assert.assertEquals(response.getStatusCode(), 200);
+
+        // Verify response body is Json
+      //  Assert.assertEquals(response.contentType(), "application/json");
+        Assert.assertEquals(response.contentType(), "application/xml");
+
     }
 }
