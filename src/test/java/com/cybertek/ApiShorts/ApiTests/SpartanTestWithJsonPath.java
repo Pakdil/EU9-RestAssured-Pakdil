30630efebde57a6f0d28f981652d30442c4c2869
pakdil <eaasbb@gmail.com>
2022-12-24 14:14:18 +0300

    SpartanTestWithJsonPath

---
+++ src/test/java/com/cybertek/ApiShorts/ApiTests/SpartanTestWithJsonPath.java
@@ -0,0 +1,53 @@
+package com.cybertek.ApiShorts.ApiTests;
+
+import static io.restassured.RestAssured.*;
+
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+import static org.testng.Assert.*;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class SpartanTestWithJsonPath {
+
+    @BeforeClass
+    public void setUpClass() {
+        baseURI = "http://54.234.104.66:8000";
+    }
+
+    @Test
+    public void test1() {
+        Response response = given().accept(ContentType.JSON)
+                .pathParam("id", 11)
+                .when().get("/api/spartans/{id}");
+
+        assertEquals(response.statusCode(), 200);
+// how to read value with path() method
+        int id = response.path("id");
+        System.out.println("id = " + id);
+
+        //how to read value with JsonPath()
+
+        JsonPath jsonData = response.jsonPath();
+
+        int id1 = jsonData.getInt("id");
+        String name = jsonData.getString("name");
+        String gender = jsonData.getString("gender");
+        long phone = jsonData.getLong("phone");
+
+        System.out.println("id1 = " + id1);
+        System.out.println("name = " + name);
+        System.out.println("gender = " + gender);
+        System.out.println("phone = " + phone);
+
+        // verify json payload with jsonPath
+        assertEquals(id1, 11);
+        assertEquals(name, "Nona");
+        assertEquals(gender, "Female");
+        assertEquals(phone, 7959094216l);
+    }
+
+}
